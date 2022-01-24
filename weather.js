1593a77fe2f8858d4bb7e23a48fd8ae301c8695b
DwayneArmster <54452726+DwayneArmster@users.noreply.github.com>
2022-01-24 08:39:31 -0600

    Updated comments for existing features.

--- weather.js
+++ weather.js
@@ -1,11 +1,11 @@
 const apikey = "3265874a2c77ae4a04bb96236a642d2f";
-
+// defined main elements of weather map
 const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
-
+// used to allow access to cities, so current weather data can be pulled from openweathermap.org
 const url =(city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
-
+// code used to retrieve specific temp for the city name submitted in the form
 async function getWeatherByLocation(city) {
         const resp = await fetch(url(city), {
         origin: "cors"});
@@ -19,7 +19,7 @@ function addWeatherToPage(data) {
 
        const weather = document.createElement('div');
        weather.classList.add('weather');
-
+// added image for visual decription of weather temperature
        weather.innerHTML =`
        <h2>${temp}°C</h2>
        <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" />
